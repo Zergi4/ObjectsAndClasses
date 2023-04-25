046d1d8b314a527ff9dc7f8ae3d8d785ff6a8148
Serg <ksyssau@ya.ru>
2023-04-25 22:06:42 +0300

    6-7

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,13 @@
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
+        Author author1 = new Author("Fedor", "Dostoevskiy");
+        Author author2 = new Author("Alber", "Kamu");
+        Book bookIdiot = new Book("Idiot", author1, 1868);
+        Book bookChuma = new Book("Chuma", author2, 1947);
 
-
-        }
+        bookIdiot.setYearPublication(2000);
+        System.out.println("bookIdiot.getYearPublication() = " + bookIdiot.getYearPublication());
     }
+
+}
