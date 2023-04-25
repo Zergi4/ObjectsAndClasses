28d8e8518200be0b41d5021ac2cdbf71a6028b3a
Serg <ksyssau@ya.ru>
2023-04-25 22:11:22 +0300

    6-7

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,7 @@ public class Main {
         Author author2 = new Author("Alber", "Kamu");
         Book bookIdiot = new Book("Idiot", author1, 1868);
         Book bookChuma = new Book("Chuma", author2, 1947);
-
+//
         bookIdiot.setYearPublication(2000);
         System.out.println("bookIdiot.getYearPublication() = " + bookIdiot.getYearPublication());
     }
